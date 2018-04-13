704f85e59156a372ee3ab70d94e71880f0cba78a
hung211097 <1512227@student.hcmus.edu.vn>
2018-04-14 00:17:28 +0700

    - Chỉnh lai li.active cho navigation bar item
    - Sticky navigation bar (bootstrap)
    - Thêm carousel (bootstrap)
    - Thêm 2 banner
    - Thêm sản 10 phẩm nổi bật (có nhiều lượt xem nhất) -> dùng Owl Slider
    - Thêm sản 11 phẩm mới nhất -> dùng Owl Slider
    - Responsive cho các thành phần được thêm vào

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+jQuery(document).ready(function($) {
+  var owl = $('.owl-carousel');
+  owl.on('initialize.owl.carousel initialized.owl.carousel ' +
+    'initialize.owl.carousel initialize.owl.carousel ' +
+    'resize.owl.carousel resized.owl.carousel ' +
+    'refresh.owl.carousel refreshed.owl.carousel ' +
+    'update.owl.carousel updated.owl.carousel ' +
+    'drag.owl.carousel dragged.owl.carousel ' +
+    'translate.owl.carousel translated.owl.carousel ' +
+    'to.owl.carousel changed.owl.carousel',
+    function(e) {
+      $('.' + e.type)
+        .removeClass('secondary')
+        .addClass('success');
+      window.setTimeout(function() {
+        $('.' + e.type)
+          .removeClass('success')
+          .addClass('secondary');
+      }, 500);
+    });
+  owl.owlCarousel({
+    loop: true,
+    nav: true,
+    items: 4,
+    dots: false,
+    navText:['<i class="fa fa-angle-left">', '<i class="fa fa-angle-right">'],
+    lazyLoad: false,
+    margin: 30,
+    video: false,
+    autoWidth: true,
+    // responsive: {
+    //   0: {
+    //     items: 1
+    //   },
+    //   600: {
+    //     items: 2
+    //   },
+    //   950: {
+    //     items: 2
+    //   },
+    //   1185: {
+    //     items: 2
+    //   },
+    //   1300: {
+    //     items: 4
+    //   }
+    // }
+  });
 });
