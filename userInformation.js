5ee0f9dceef5061e138b62cb43d8b8d34f7599cf
NTHungFitHcmus <nthungfithcmus@gmail.com>
2018-04-19 01:09:33 +0700

    Sửa thanh bên trái thành menu bar, giao diện phần thông tin cá nhân (UserInformation.html)

--- userInformation.js
+++ userInformation.js
@@ -5,3 +5,11 @@
  */
 
 $('#InfoUserBtn').text(localStorage.getItem('username'));
+
+$('#infoName').html(localStorage.getItem('username').substring(9, localStorage.getItem('username').length - 1)
+        + `<a href="javascript:;"><span class="fas fa-edit" aria-hidden="true" style="float: right;"></span></a>`);
+
+$('.vertical-menu li a').on('click', function() {
+    $('.vertical-menu a').removeClass();
+    $(this).addClass('active');
+});
